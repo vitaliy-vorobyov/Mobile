a33fca3942be9bab9dc2502381de0287f0fb3b01
vitaliy-vorobyov <donsteel@rambler.ru>
2016-05-25 13:02:58 +0300

    Commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<?php 
+	require('layout/header.php');
+?>
+
+
+<section class="promo">
+	
+	<div class="promo__inner">
+		<a class="promo__logo" href="/"><img src="images/logo.png"></a>
+
+ 		<p class="promo__text">
+			A great new free psd theme to showcase your new application. 
+		</p>
+
+		<div class="promo__links">
+			<a class="promo__link" href="#"><i class="fa fa-apple" aria-hidden="true"></i></a>
+			<a class="promo__link" href="#"><i class="fa fa-android" aria-hidden="true"></i></a>
+			<a class="promo__link" href="#"><i class="fa fa-windows" aria-hidden="true"></i></a>
+		</div> 
+
+	</div>
+
+</section>
+<section class="features">
+	
+	<div class="container">
+	
+		<div class="row">
+
+			<div class="feature col-4 text-center">
+
+				<i class="feature__icon fa fa-cog"></i>
+				<h4 class="feature__title">Editable Theme</h4>
+				<p class="feature__content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
+				</p>
+
+			</div>
+
+			<div class="feature col-4 text-center">
+
+				<i class="feature__icon fa fa-star"></i>
+				<h4 class="feature__title">Flat Design</h4>
+				<p class="feature__content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
+				</p>
+
+			</div>
+
+			<div class="feature col-4 text-center">
+
+				<i class="feature__icon fa fa-globe"></i>
+				<h4 class="feature__title">Reach Your Audience</h4>
+				<p class="feature__content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
+				</p>
+				
+			</div>
+		</div>
+
+	</div>
+
+</section>
+
+<section class="subscribe">
+	
+	<div class="container">
+	
+		<div class="row">
+
+			<div class="col-6">
+
+				<h3 class="subscribe__title">Get notified by any updates!</h3>
+				<p class="subscribe_text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam fugiat distinctio atque quis maxime necessitatibus iusto amet tenetur nulla laborum ab ullam, non architecto soluta, id cupiditate perspiciatis vero totam!
+				</p>
+				<form action="">
+					
+					<fieldset class="subscribe__fieldset">
+						<input type="email" placeholder="Enter your email ...">
+						<button onclick="return false;">Notify</button>
+					</fieldset>	
+
+				</form>
+			</div>
+
+			<div class="col-6">
+
+				<div class="video__wrapper">
+					<iframe class="video__iframe" src="https://player.vimeo.com/video/167054481" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
+
+<section class="social">
+	
+	<div class="container">
+		<blockquote>
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum velit vitae nulla molestie eu commodo nulla dapib us. Donec id sollicitudin urna. Integer at dui ac leo fermentum varius eleifend ut."</p>
+			<cite>Jane Hunt</cite>
+		</blockquote>
+		<div class="social__inner">
+			<div class="line">
+				<div class="social__photo">
+						<a href="#"><img src="/images/1.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/2.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/3.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/4.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/5.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/6.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/7.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/8.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/9.jpg"></a>
+				</div>
+				<div class="social__photo">			
+						<a href="#"><img src="/images/10.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/11.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/12.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/13.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/14.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/15.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/16.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/17.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/18.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/19.jpg"></a>
+				</div>
+				<div class="social__photo display_none">
+						<a href="#"><img src="/images/20.jpg"></a>
+				</div>
+				<div class="social__photo">
+						<a href="#"><img src="/images/21.jpg"></a>
+				</div>
+			</div>	
+		</div>
+		<div class="social__inner">
+			<h3>Say Hi & Get in Touch</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.</p>
+			<div class="line">
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-twitter" href="#"></a>
+				</div>
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-facebook" href="#"></a>
+				</div>				
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-pinterest" href="#"></a>
+				</div>				
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-google-plus" href="#"></a>
+				</div>				
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-linkedin" href="#"></a>
+				</div>				
+				<div class="social__icon">
+					<a class="social__icon__inner fa fa-youtube" href="#"></a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</section>
+<footer>
+	<ul>
+		<li>
+			<a href="#">Contact</a> 
+		</li>
+		<li>
+			<a href="#">Download</a> 
+		</li>
+		<li>
+			<a href="#">Press</a> 
+		</li>
+		<li>
+			<a href="#">Email</a> 
+		</li>
+		<li>
+			<a href="#">Support</a> 
+		</li>
+		<li>
+			<a href="#">Privacy Policy</a> 
+		</li>
+	</ul>
+</footer>
+
+
+<?php 
+	require('layout/footer.php');
+?>
---
+++ layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Mobile app</title>
+
+		<link href="css/normalize.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	</head>
+	<body>
